be0581d15732fe66a245bc5ef2bd8a1966a46da9
Kevin Pandya <kevinpandya02@gmail.com>
2023-01-02 21:25:02 +0530

    Add project files.

---
+++ RazorCRUD/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorCRUD.Data;
+using RazorCRUD.Model;
+
+namespace RazorCRUD.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorCRUD.Data.RazorCRUDContext _context;
+
+        public DetailsModel(RazorCRUD.Data.RazorCRUDContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
